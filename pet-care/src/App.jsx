2b26134262de3d755eea47e56df318242fe4a0da
osparking <jbpark03@gmail.com>
2024-10-21 21:47:49 +0900

    include Home component from within App

--- pet-care/src/App.jsx
+++ pet-care/src/App.jsx
@@ -1,12 +1,13 @@
 import './App.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
+import Home from './components/home/Home'
 
 function App() {
   return (
     <main className=''>
       
       <div className='text-info'>새로운 프로젝트에서 환영합니다.</div>      
-      
+      <Home/>
     </main>
   )
 }
