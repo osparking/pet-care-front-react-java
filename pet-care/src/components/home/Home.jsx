e9c0a3b1734b168ee3e3654ce1260d861397ad9f
osparking <jbpark03@gmail.com>
2024-10-23 09:40:23 +0900

    insert second card w/ detail services

--- pet-care/src/components/home/Home.jsx
+++ pet-care/src/components/home/Home.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import d5 from "../../assets/images/d5.jpg";
 import vett from "../../assets/images/vett.jpg";
-import {Col, Row, Button, Card, Container} from 'react-bootstrap';
+import {Col, Row, Button, Card, Container, ListGroup} from 'react-bootstrap';
 
 const Home = () => {
   return (
@@ -12,8 +12,9 @@ const Home = () => {
             <Card.Img
               variant='top'
               src={d5}
-              alt='우리 소개'/>
+              alt='저희 소개'/>
             <Card.Body>
+              <h2 className='text-info'>저희들 소개</h2>
               <Card.Title>당신의 털복숭이 친구를 친절히 보살펴요</Card.Title>
               <Card.Text>
                 At Universal Pet Care, we believe every pet deserves the best.
@@ -35,6 +36,29 @@ const Home = () => {
           </Card>        
         </Col>
         <Col>
+          <Card>
+            <Card.Img 
+              variant='top'
+              src={vett}
+              alt='저희들 소개' />
+            <Card.Body>
+              <h2 className='text-info'>서비스 주제</h2>
+              <Card.Title>저희들 직무</Card.Title>
+              <ListGroup variant='flush'>
+                <ListGroup.Item>애완동물 건강 검진</ListGroup.Item>
+                <ListGroup.Item>긴급 상황 수술</ListGroup.Item>
+                <ListGroup.Item>팻 백신 주사</ListGroup.Item>
+                <ListGroup.Item>치과적 치료</ListGroup.Item>
+                <ListGroup.Item>Spaying and Neutering</ListGroup.Item>
+                <ListGroup.Item>And many more...</ListGroup.Item>
+              </ListGroup>
+              <Card.Text className='mt-3'>
+                From routine check-ups to emergency surgery, our full range of
+                veterinary services ensures your pet's health is in good hands.
+              </Card.Text>
+              <Button variant='outline-info'>우리 수의사들을 만나보세요</Button>
+            </Card.Body>
+          </Card>
         </Col>
       </Row>
     </Container>
