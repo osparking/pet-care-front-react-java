f0339fd924e3c561851a6212e60a37ff8856dd69
osparking <jbpark03@gmail.com>
2024-11-24 21:26:44 +0900

    add background image slider onto root layout

--- pet-care/src/components/layouts/RootLayout.jsx
+++ pet-care/src/components/layouts/RootLayout.jsx
@@ -1,8 +1,10 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
+import BackgroundImageSlider from "../common/BackgroundImageSlider";
 const RootLayout = () => {
   return (
     <main>
+      <BackgroundImageSlider />
       <div>
         <Outlet />
       </div>
