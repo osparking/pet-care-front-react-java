7487f531f390f764188d52aa7f2e93dc235123f8
osparking <jbpark03@gmail.com>
2024-11-24 10:25:34 +0900

    background image carousel slider w/ 2 varis

---
+++ pet-care/src/components/common/BackgroundImageSlider.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import bg from "../../assets/images/bg.png";
+import bg1 from "../../assets/images/bg1.png";
+import bg3 from "../../assets/images/bg3.png";
+
+const BackgroundImageSlider = () => {
+  const backgrounds = [bg1, bg, bg3];
+  const [index, setIndex] = useState(0);
+  return <div></div>;
+};
+
+export default BackgroundImageSlider;
