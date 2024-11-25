8f9b0ae670ad3e440961ebcf91dfc519863b4200
osparking <jbpark03@gmail.com>
2024-11-25 13:22:11 +0900

    add navi bar into root layout

--- pet-care/src/components/layouts/RootLayout.jsx
+++ pet-care/src/components/layouts/RootLayout.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
 import BackgroundImageSlider from "../common/BackgroundImageSlider";
+import NavBar from "./NavBar";
 const RootLayout = () => {
   return (
     <main>
+      <NavBar />
       <BackgroundImageSlider />
       <div>
         <Outlet />
