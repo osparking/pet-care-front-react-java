ef20b5490f6a82a9340fb0df9250d1eba5a104fc
osparking <jbpark03@gmail.com>
2024-11-29 10:39:28 +0900

    fix get function actual URL param

--- pet-care/src/components/veterinarians/VetService.jsx
+++ pet-care/src/components/veterinarians/VetService.jsx
@@ -2,7 +2,7 @@ import { api } from "../utils/api";
 
 export async function getVets() {
   try {
-    const result = await api.get("/vets/get-all-vets");
+    const result = await api.get("/vets/get_all_vets");
     return result.data;
   } catch (err) {
     throw err;
