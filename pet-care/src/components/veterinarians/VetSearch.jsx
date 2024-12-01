2afad28f19e6c53050f105fe3fe0daf60436c8e5
osparking <jbpark03@gmail.com>
2024-12-01 12:29:24 +0900

    Vet search compo' w/ search key state

---
+++ pet-care/src/components/veterinarians/VetSearch.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const VetSearch = () => {
+  const [searchKey, setSearchKey] = useState({
+    date: null,
+    time: null,
+    specialties: "",
+  });
+  return <div></div>;
+};
+
+export default VetSearch;
