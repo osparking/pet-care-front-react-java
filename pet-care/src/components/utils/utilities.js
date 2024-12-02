3af120d5581891144a5955f082c548b03151b1ad
osparking <jbpark03@gmail.com>
2024-12-02 09:39:36 +0900

    useAlertWithTimeout custom hook

---
+++ pet-care/src/components/utils/utilities.js
@@ -0,0 +1,18 @@
+import { useEffect } from "react";
+
+export const useAlertWithTimeout = (
+  initialVisibility = false,
+  duration = 10000
+) => {
+  const [isVisible, setIsVisible] = useState(initialVisibility);
+
+  useEffect(() => {
+    let timer;
+    if (isVisible) {
+      timer = setTimeout(() => setIsVisible(false), duration);
+    }
+    return () => clearTimeout(timer);
+  }, [isVisible, duration]);
+
+  return [isVisible, setIsVisible];
+};
