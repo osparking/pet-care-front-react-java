4ccf5a8cfa318276eff7da5dc6a9be8c957543f2
osparking <jbpark03@gmail.com>
2024-12-08 09:54:51 +0900

    modal return value skeleton created

--- pet-care/src/components/modals/AddItemModal.jsx
+++ pet-care/src/components/modals/AddItemModal.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Modal } from "react-bootstrap";
 
 const AddItemModal = ({ show, closer, saver, label }) => {
   const [itemValue, setItemValue] = useState("");
@@ -7,7 +8,20 @@ const AddItemModal = ({ show, closer, saver, label }) => {
     setItemValue("");
     closer();
   };
-  return <div></div>;
+  return (
+    <Modal>
+      <Modal.Header></Modal.Header>
+      <Modal.Body>
+        <Form>
+          <Form.Group>
+            <Form.Label></Form.Label>
+            <Form.Control></Form.Control>
+          </Form.Group>
+        </Form>
+      </Modal.Body>
+      <Modal.Footer></Modal.Footer>
+    </Modal>
+  );
 };
 
 export default AddItemModal;
