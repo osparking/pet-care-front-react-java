431501a394f71b9460e34544efdb930cb3f3ac8b
osparking <jbpark03@gmail.com>
2024-12-08 19:07:03 +0900

    pet legend index number

--- pet-care/src/components/pet/PetEntry.jsx
+++ pet-care/src/components/pet/PetEntry.jsx
@@ -1,7 +1,14 @@
 import React from "react";
 
 const PetEntry = ({ pet, index, removePet, canRemove, handleInputChange }) => {
-  return <div>PetEntry</div>;
+  return (
+    <fieldset>
+      <legend className="legend">{`팻 #${색인 + 1} 정보`}</legend>
+      <Form.Group>
+        <Form.Control></Form.Control>
+      </Form.Group>
+    </fieldset>
+  );
 };
 
 export default PetEntry;
