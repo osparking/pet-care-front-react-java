5ecfaae3b7faa200da45d7af4fd3faabe7e44e3c
osparking <jbpark03@gmail.com>
2024-12-08 13:19:43 +0900

    function handles color change

--- pet-care/src/components/pet/PetColor.jsx
+++ pet-care/src/components/pet/PetColor.jsx
@@ -5,6 +5,13 @@ const PetColor = ({ value, onChange }) => {
   const [showModal, setShowModal] = useState(false);
 
   // 1. 색상 변화 처리
+  const handleColorChange = (event) => {
+    if (event.target.value === "add-new-item") {
+      setShowModal(true);
+    } else {
+      onChange(event);
+    }
+  };
   // 2. 새 항목 저장 처리
 
   return <div>PetColor</div>;
