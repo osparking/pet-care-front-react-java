ce8e94a91c2d779880e79d3f0a36039ff626f0f9
osparking <jbpark03@gmail.com>
2024-12-09 14:41:17 +0900

    appointment form compo - phase 1

---
+++ pet-care/src/components/appointment/BookAppointment.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const BookAppointment = () => {
+  const [formData, setFormData] = useState({
+    name: "",
+    pet: "",
+    date: "",
+    time: "",
+    message: "",
+  });
+  return <div>BookAppointment</div>;
+};
+
+export default BookAppointment;
