6f5d7de9ac5395ac74696972c2def8d2780501aa
osparking <jbpark03@gmail.com>
2024-12-11 19:39:02 +0900

    fine tune top margin of fieldset

--- pet-care/src/components/pet/PetEntry.jsx
+++ pet-care/src/components/pet/PetEntry.jsx
@@ -14,7 +14,7 @@ import PetType from "./PetType";
 
 const PetEntry = ({ pet, index, removePet, canRemove, handleInputChange }) => {
   return (
-    <fieldset>
+    <fieldset className="field-set mt-3">
       <legend className="legend">{`팻 #${index + 1} 정보`}</legend>
       <fieldset className="mb-2">
         <Form.Group as={Row}>
