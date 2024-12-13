d299537cb0542dcf8eff326ef44b41c373952449
osparking <jbpark03@gmail.com>
2024-12-13 11:14:46 +0900

    fetches pet colors and breeds from backend

--- pet-care/src/components/pet/PetService.jsx
+++ pet-care/src/components/pet/PetService.jsx
@@ -1,10 +1,28 @@
 import { api } from "../utils/api";
 
 export async function getPetTypes() {
-  try {
-    const result = await api.get("/pets/get_types");
-    return result.data;
-  } catch (error) {
-    throw error;
+    try {
+      const result = await api.get("/pets/get_types");
+      return result.data;
+    } catch (error) {
+      throw error;
+    }
   }
-}
+
+  export async function getPetColors() {
+    try {
+      const result = await api.get("/pets/get_colors");
+      return result.data;
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  export async function getPetBreeds(type) {
+    try {
+      const result = await api.get("/pets/get_breeds");
+      return result.data;
+    } catch (error) {
+      throw error;
+    }
+  }
