d28c3319ef1abed8cb10a4371381a429a0e55f04
osparking <jbpark03@gmail.com>
2024-12-14 11:43:24 +0900

    in pet breeds read, change T/F branch code

--- pet-care/src/components/pet/PetService.js
+++ pet-care/src/components/pet/PetService.js
@@ -1,33 +1,33 @@
 import { api } from "../utils/api";
 
 export async function getPetTypes() {
-    try {
-      const result = await api.get("/pets/get_types");
-      return result.data;
-    } catch (error) {
-      throw error;
-    }
+  try {
+    const result = await api.get("/pets/get_types");
+    return result.data;
+  } catch (error) {
+    throw error;
   }
+}
 
-  export async function getPetColors() {
-    try {
-      const result = await api.get("/pets/get_colors");
-      return result.data;
-    } catch (error) {
-      throw error;
-    }
+export async function getPetColors() {
+  try {
+    const result = await api.get("/pets/get_colors");
+    return result.data;
+  } catch (error) {
+    throw error;
   }
+}
 
-  export async function getPetBreeds(type = "") {
-    try {
-      let result;
-      if (type === "") {
-        result = await api.get(`/pets/get_breeds?type=${type}`);
-      } else {
-        result = await api.get("/pets/get_breeds");
-      }
-      return result.data;
-    } catch (error) {
-      throw error;
+export async function getPetBreeds(type = "") {
+  try {
+    let result;
+    if (type === "") {
+      result = await api.get("/pets/get_breeds");
+    } else {
+      result = await api.get(`/pets/get_breeds?type=${type}`);
     }
+    return result.data;
+  } catch (error) {
+    throw error;
   }
+}
