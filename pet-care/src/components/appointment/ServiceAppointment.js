77153b26c6ee26a0362b221c3e259f455d2dcc54
osparking <jbpark03@gmail.com>
2024-12-14 14:44:32 +0900

    front-end bookAppointment function body

--- pet-care/src/components/appointment/ServiceAppointment.js
+++ pet-care/src/components/appointment/ServiceAppointment.js
@@ -1,4 +1,13 @@
 import { api } from "../utils/api";
 
-export async function bookAppointment({senderId, recipientId, request}) {
+export async function bookAppointment({ senderId, recipientId, request }) {
+  try {
+    const result = await api.post(
+      `/appointments/book_appointment/${senderId}/${recipientId}`,
+      request
+    );
+    return result.data;
+  } catch (err) {
+    throw err;
+  }
 }
