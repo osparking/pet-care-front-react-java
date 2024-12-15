85be930b51152fdf72d3d3410763e5a1078437d6
osparking <jbpark03@gmail.com>
2024-12-15 18:55:35 +0900

    match appoint booking request path to backend

--- pet-care/src/components/appointment/ServiceAppointment.js
+++ pet-care/src/components/appointment/ServiceAppointment.js
@@ -1,9 +1,9 @@
 import { api } from "../utils/api";
 
-export async function bookAppointment({ senderId, recipientId, request }) {
+export async function bookAppointment(senderId, recipientId, request) {
   try {
     const result = await api.post(
-      `/appointments/book_appointment/${senderId}/${recipientId}`,
+      `/appointments/create?senderId=${senderId}&recipientId=${recipientId}`,
       request
     );
     return result.data;
