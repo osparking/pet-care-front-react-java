26cef7b2688f2f0e0938853460ecbb1691e78932
osparking <jbpark03@gmail.com>
2024-12-17 08:13:44 +0900

    process spinner definition - step 1

---
+++ pet-care/src/components/common/ProcessSpinner.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const ProcessSpinner = ({
+  size = "sm",
+  animation = "border",
+  message = "",
+}) => {
+  return <div>ProcessSpinner</div>;
+};
+
+export default ProcessSpinner;
