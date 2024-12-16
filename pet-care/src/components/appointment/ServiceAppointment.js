6ea41b33dc9f873e524e858314482df3dedb2eb9
osparking <jbpark03@gmail.com>
2024-12-16 14:55:15 +0900

    use wrong recipient/vet ID of 10

--- pet-care/src/components/appointment/ServiceAppointment.js
+++ pet-care/src/components/appointment/ServiceAppointment.js
@@ -3,7 +3,7 @@ import { api } from "../utils/api";
 export async function bookAppointment(senderId, recipientId, request) {
   try {
     const result = await api.post(
-      `/appointments/create?senderId=${senderId}&recipientId=${recipientId}`,
+      `/appointments/create?senderId=${senderId}&recipientId=10`,
       request
     );
     return result.data;
