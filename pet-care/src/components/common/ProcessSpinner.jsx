2b6a1536a9c5114a77baa89de53d14967dca848e
osparking <jbpark03@gmail.com>
2024-12-17 08:47:13 +0900

    insert Spinner and set props

--- pet-care/src/components/common/ProcessSpinner.jsx
+++ pet-care/src/components/common/ProcessSpinner.jsx
@@ -1,11 +1,22 @@
 import React from "react";
+import Spinner from "react-bootstrap";
 
 const ProcessSpinner = ({
   size = "sm",
   animation = "border",
   message = "",
 }) => {
-  return <div>ProcessSpinner</div>;
+  return (
+    <div>
+      <Spinner
+        as="span"
+        animation={animation}
+        size={size}
+        role="status"
+        area-hidden="true"
+      />
+    </div>
+  );
 };
 
 export default ProcessSpinner;
