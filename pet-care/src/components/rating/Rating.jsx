ab1df07d0cf2005dcbde43f557b8b167a178a6d3
osparking <jbpark03@gmail.com>
2024-12-20 11:51:29 +0900

    define hover and review(rating, comment) states

--- pet-care/src/components/rating/Rating.jsx
+++ pet-care/src/components/rating/Rating.jsx
@@ -1,6 +1,12 @@
 import React from "react";
 
 const Rating = ({ vetId, onReviewSubmit }) => {
+  const [hover, setHover] = useState(null);
+  const [review, setReview] = useState({
+    rating: null,
+    comment: null,
+  });
+
   return <div>Rating</div>;
 };
 
