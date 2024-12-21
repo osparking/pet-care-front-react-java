379624f6c54fcef9be2440fb43d985f070fb51df
osparking <jbpark03@gmail.com>
2024-12-22 05:42:34 +0900

    isLoading state, vetId param

--- pet-care/src/components/veterinarians/Vet.jsx
+++ pet-care/src/components/veterinarians/Vet.jsx
@@ -1,7 +1,10 @@
 import React, { useState } from "react";
+import { useParams } from "react-router-dom";
 
 const Vet = () => {
   const [vet, setVet] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+  const { vetId } = useParams();
 
   return <div>Vet</div>;
 };
