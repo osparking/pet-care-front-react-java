0c46c038f05210dc1b11a8caf67d39d8b3da9a99
osparking <jbpark03@gmail.com>
2024-12-21 15:08:29 +0900

    reviewed patient or vet name display code

--- pet-care/src/components/review/Review.jsx
+++ pet-care/src/components/review/Review.jsx
@@ -1,6 +1,11 @@
 import React from "react";
+import { UserType } from "../utils/utilities";
 
 const Review = ({ review, userType }) => {
+  const ratedDoctor =
+    userType === UserType.PATIENT
+      ? `당신은 ${review.vetName} 수의사의 진료를 평가하였습니다.`
+      : `${review.patientName} 고객이 입력한 리뷰입니다.`;
   return <div>Review</div>;
 };
 
