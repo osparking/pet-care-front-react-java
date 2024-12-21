716f311325e6b5da491e838cdbe8b4ff3d7e2ba5
osparking <jbpark03@gmail.com>
2024-12-21 11:37:32 +0900

    star rating average compo - skeleton

---
+++ pet-care/src/components/rating/RatingAvg.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { FaStar, FaStarHalfAlt, FaRegStar } from 'react-icons/fa';
+
+const RatingAvg = () => {
+  return (
+    <div>RatingAvg</div>
+  )
+}
+
+export default RatingAvg
