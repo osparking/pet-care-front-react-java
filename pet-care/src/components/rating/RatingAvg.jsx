e82f4d696cfaf0771efc4b4540108b6add259e19
osparking <jbpark03@gmail.com>
2024-12-21 12:09:51 +0900

    insert half star conditionally

--- pet-care/src/components/rating/RatingAvg.jsx
+++ pet-care/src/components/rating/RatingAvg.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { FaStar } from "react-icons/fa";
+import { FaStar, FaStarHalfAlt } from "react-icons/fa";
 
 const RatingAvg = ({ rating }) => {
   const starsMax = 5;
@@ -8,6 +8,12 @@ const RatingAvg = ({ rating }) => {
   // 찬 별을 rating의 정수부 만큼 삽입
   for (let i = 0; i < Math.floor(rating); i++) {
     stars.push(<FaStar key={i} color="#ffc107" />);
+  }
+
+  // 반 별을 조건부로 하나 삽입
+  // 소수점 이하 값이 0.25 이상이면 반별 부여.
+  if (rating % 1 >= 0.25) {
+    stars.push(<FaStarHalfAlt key="half" color="#ffc107" />);
   }
 
   return <div>RatingAvg</div>;
