740d61cd79109c399d13b2d912cf0f19f16d3565
osparking <jbpark03@gmail.com>
2024-12-23 08:01:49 +0900

    prepend URL part for the controller class

--- pet-care/src/components/user/UserService.js
+++ pet-care/src/components/user/UserService.js
@@ -2,7 +2,7 @@ import { api } from "../utils/api";
 
 export async function getUserById(userId) {
   try {
-    const result = await api.get(`/user/${userId}`);
+    const result = await api.get(`/users/user/${userId}`);
     return result.data;
   } catch (err) {
     throw err;
