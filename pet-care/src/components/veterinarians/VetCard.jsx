e20f7bbff90884de8c0c80407bc8e8365d8228ac
osparking <jbpark03@gmail.com>
2024-12-23 19:34:12 +0900

    display real average star count on vet card

--- pet-care/src/components/veterinarians/VetCard.jsx
+++ pet-care/src/components/veterinarians/VetCard.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Accordion, Card, Col } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import ImageUser from "../common/ImageUser";
+import RatingAvg from "../rating/RatingAvg";
 
 const VetCard = ({ vet }) => {
   return (
@@ -22,7 +23,7 @@ const VetCard = ({ vet }) => {
                 <h6>전문분야: {vet.specialization}</h6>
               </Card.Title>
               <Card.Text className="review rating-stars">
-                리뷰: 별 여러 개
+                리뷰: <RatingAvg rating={vet.averageRating} />{"/ "}{vet.totalReviewers}
               </Card.Text>
               <Link to={`/appointments/create/${vet.id}`} className="link">
                 진료 예약
