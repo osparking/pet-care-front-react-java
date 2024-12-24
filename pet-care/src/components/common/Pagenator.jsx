3d85f7121cd80ad842b1e25b145e3ad03525eeee
osparking <jbpark03@gmail.com>
2024-12-24 21:26:26 +0900

    use Pagination from react-bootstrap

--- pet-care/src/components/common/Pagenator.jsx
+++ pet-care/src/components/common/Pagenator.jsx
@@ -1,9 +1,21 @@
 import React from "react";
+import { Pagination } from "react-bootstrap";
 
-const Pagenator = ({ pageSize, totalItems, currPage, pagenate }) => {
+const Pagenator = ({ pageSize, totalItems, currPage, paginate }) => {
   let active = currPage;
   let items = [];
-  for (let i = 1; i <= Math.ceil(totalItems / pageSize); i++) {}
+  for (let i = 1; i <= Math.ceil(totalItems / pageSize); i++) {
+    items.push(
+      <Pagination.Item
+        key={i}
+        active={i === active}
+        onClick={() => paginate(i)}
+      >
+        {i}
+      </Pagination.Item>
+    );
+    active = i;
+  }
   return <div>Pagenator</div>;
 };
 
