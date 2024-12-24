73e64fe98ae68a12552177951af0970ca4c1a520
osparking <jbpark03@gmail.com>
2024-12-24 21:20:02 +0900

    first 3 lines of Pagenator

--- pet-care/src/components/common/Pagenator.jsx
+++ pet-care/src/components/common/Pagenator.jsx
@@ -1,6 +1,9 @@
 import React from "react";
 
 const Pagenator = ({ pageSize, totalItems, currPage, pagenate }) => {
+  let active = currPage;
+  let items = [];
+  for (let i = 1; i <= Math.ceil(totalItems / pageSize); i++) {}
   return <div>Pagenator</div>;
 };
 
