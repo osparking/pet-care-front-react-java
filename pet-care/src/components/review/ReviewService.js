3c36dd447e0617780d8cdb74e65f6d18140d665b
osparking <jbpark03@gmail.com>
2024-12-24 12:11:27 +0900

    fully imple' saveReview function

--- pet-care/src/components/review/ReviewService.js
+++ pet-care/src/components/review/ReviewService.js
@@ -1,1 +1,11 @@
-export async function saveReview(vetId, reviewerId, review) {}
+export async function saveReview(patId, vetId, review) {
+  try {
+    const response = await api.post(
+      `reviews/create?patientId=${patId}&vetId=${vetId}`,
+      review
+    );
+    return response.data;
+  } catch (error) {
+    throw error;
+  }
+}
