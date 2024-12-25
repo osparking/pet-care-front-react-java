0da8036f4100258962fc8e52dbeb959b8610b748
osparking <jbpark03@gmail.com>
2024-12-25 12:50:04 +0900

    user registration component - skeleton

---
+++ pet-care/src/components/user/UserRegist.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+const UserRegist = () => {
+  const [user, setUser] = useState({
+    lastName: "",
+  });
+  return <div>UserRegist</div>;
+};
+
+export default UserRegist;
