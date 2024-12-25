f0c56ac04ce6b1d2cffac9989d8e9518c5f63bac
osparking <jbpark03@gmail.com>
2024-12-25 12:52:35 +0900

    firstName and 3 more fields

--- pet-care/src/components/user/UserRegist.jsx
+++ pet-care/src/components/user/UserRegist.jsx
@@ -3,6 +3,10 @@ import React from "react";
 const UserRegist = () => {
   const [user, setUser] = useState({
     lastName: "",
+    firstName: "",
+    gender: "",
+    phone: "",
+    email: "",
   });
   return <div>UserRegist</div>;
 };
