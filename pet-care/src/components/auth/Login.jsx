3ff1fd971d820910f989702b15ad5705d6db4d02
osparking <jbpark03@gmail.com>
2024-12-26 17:01:55 +0900

    login compo - 1st constr step

---
+++ pet-care/src/components/auth/Login.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Login = () => {
+  const [creden, setCreden] = useState({
+    email: "",
+    password: "",
+  });
+  return <div>Login</div>;
+};
+
+export default Login;
