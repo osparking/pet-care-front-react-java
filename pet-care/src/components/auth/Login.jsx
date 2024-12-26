88186049eb982b9c48e6e5312a1616e888f71871
osparking <jbpark03@gmail.com>
2024-12-26 19:51:20 +0900

    move closing card.title tag upward

--- pet-care/src/components/auth/Login.jsx
+++ pet-care/src/components/auth/Login.jsx
@@ -18,47 +18,46 @@ const Login = () => {
         <Col sm={6}>
           <Card>
             <Card.Body>
-              <Card.Title className="text-center mb-4">
-                <Form>
-                  <Form.Group className="mb-3" controlId="username">
-                    <Form.Label>이메일</Form.Label>
-                    <InputGroup>
-                      <InputGroup.Text>
-                        <BsPersonFill />
-                      </InputGroup.Text>
-                      <Form.Control
-                        type="email"
-                        name="email"
-                        placeholder="(이메일)"
-                        value={creden.email}
-                        onChange={handleInputChange}
-                      />
-                    </InputGroup>
-                  </Form.Group>
-                  <Form.Group className="mb-3" controlId="password">
-                    <Form.Label>비밀번호</Form.Label>
-                    <InputGroup>
-                      <InputGroup.Text>
-                        <BsLockFill />
-                      </InputGroup.Text>
-                      <Form.Control
-                        type="password"
-                        name="password"
-                        placeholder="(비밀번호)"
-                        value={creden.password}
-                        onChange={handleInputChange}
-                      />
-                    </InputGroup>
-                  </Form.Group>
-                  <Button
-                    variant="outline-primary"
-                    type="submit"
-                    className="w-100"
-                  >
-                    로그인
-                  </Button>
-                </Form>
-              </Card.Title>
+              <Card.Title className="text-center mb-4"></Card.Title>
+              <Form>
+                <Form.Group className="mb-3" controlId="username">
+                  <Form.Label>이메일</Form.Label>
+                  <InputGroup>
+                    <InputGroup.Text>
+                      <BsPersonFill />
+                    </InputGroup.Text>
+                    <Form.Control
+                      type="email"
+                      name="email"
+                      placeholder="(이메일)"
+                      value={creden.email}
+                      onChange={handleInputChange}
+                    />
+                  </InputGroup>
+                </Form.Group>
+                <Form.Group className="mb-3" controlId="password">
+                  <Form.Label>비밀번호</Form.Label>
+                  <InputGroup>
+                    <InputGroup.Text>
+                      <BsLockFill />
+                    </InputGroup.Text>
+                    <Form.Control
+                      type="password"
+                      name="password"
+                      placeholder="(비밀번호)"
+                      value={creden.password}
+                      onChange={handleInputChange}
+                    />
+                  </InputGroup>
+                </Form.Group>
+                <Button
+                  variant="outline-primary"
+                  type="submit"
+                  className="w-100"
+                >
+                  로그인
+                </Button>
+              </Form>
             </Card.Body>
           </Card>
         </Col>
