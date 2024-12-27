1e2327f444a08bb22a05e1d5c40d42a20c3c70e6
osparking <jbpark03@gmail.com>
2024-12-27 21:39:57 +0900

    password hide toggle icon

--- pet-care/src/components/modals/ChangePasswordModal.jsx
+++ pet-care/src/components/modals/ChangePasswordModal.jsx
@@ -1,7 +1,9 @@
 import React from "react";
+import { eyeOff } from "react-icons-kit/feather";
 
 const ChangePasswordModal = () => {
   const [type, setType] = useState("password");
+  const { icon, setIcon } = useState(eyeOff);
   return <div>ChangePasswordModal</div>;
 };
 
