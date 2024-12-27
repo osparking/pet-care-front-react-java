e0b474663192f8e012d398b025182695f0bd04f7
osparking <jbpark03@gmail.com>
2024-12-27 15:31:29 +0900

    update user photo function - 2nd upgrade

--- pet-care/src/components/photo/ImageService.jsx
+++ pet-care/src/components/photo/ImageService.jsx
@@ -1,1 +1,9 @@
-export async function updateUserPhoto(photoId, photo) {}
+import { api } from "../utils/api";
+
+export async function updateUserPhoto(photoId, photo) {
+  try {
+    const response = await api.put(`/photos/${photoId}/update`, photo, {
+      headers: { "Content-Type": "application/octet-stream" },
+    });
+  } catch (error) {}
+}
