9745e1baa7736fdfc8c537ed3e708a82dd9288f1
osparking <jbpark03@gmail.com>
2024-12-27 09:05:25 +0900

    two states - user, file

--- pet-care/src/components/common/ImageUp.jsx
+++ pet-care/src/components/common/ImageUp.jsx
@@ -3,6 +3,11 @@ import React from "react";
 const ImageUp = () => {
   // 1. 유저 읽기
   // 2. 원래 영상을 가지고 있나 검사
+  // 2-1. 있다 - 기존 영상 갱신
+  // 2-2. 없다 - 새로운 영상 받아서 업로드
+
+  const [user, setUser] = useState(null);
+  const [file, setFile] = useState(null);
 
   return <div>ImageUp</div>;
 };
