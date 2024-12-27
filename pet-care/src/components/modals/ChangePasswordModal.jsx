c6bfcfa054d8fbdc385358b351a3edc00a23abca
osparking <jbpark03@gmail.com>
2024-12-27 21:30:57 +0900

    change password modal compo - step 1

---
+++ pet-care/src/components/modals/ChangePasswordModal.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+const ChangePasswordModal = () => {
+  const [type, setType] = useState("password");
+  return <div>ChangePasswordModal</div>;
+};
+
+export default ChangePasswordModal;
