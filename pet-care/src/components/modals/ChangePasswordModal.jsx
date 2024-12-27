213693fb13bee84552b54cc65b9ee3814f0d8a08
osparking <jbpark03@gmail.com>
2024-12-27 21:45:45 +0900

    password 3 states object

--- pet-care/src/components/modals/ChangePasswordModal.jsx
+++ pet-care/src/components/modals/ChangePasswordModal.jsx
@@ -4,6 +4,12 @@ import { eyeOff } from "react-icons-kit/feather";
 const ChangePasswordModal = () => {
   const [type, setType] = useState("password");
   const { icon, setIcon } = useState(eyeOff);
+  const [pwds, setPwds] = useState({
+    curPwd: "",
+    newPwd: "",
+    cnfPwd: "",
+  });
+
   return <div>ChangePasswordModal</div>;
 };
 
