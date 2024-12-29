2f8b633f8580709a7d0bf8abac0eb8feea95b67a
osparking <jbpark03@gmail.com>
2024-12-29 18:02:06 +0900

    show/hide event handler for two modals

--- pet-care/src/components/user/UserProfile.jsx
+++ pet-care/src/components/user/UserProfile.jsx
@@ -4,6 +4,11 @@ const UserProfile = ({ user }) => {
   const [showImageUpModal, setShowImageUpModal] = useState(false);
   const [showChangePasswordModal, setShowChangePasswordModal] = useState(false);
 
+  const handleShowImageUpModal = () => setShowImageUpModal(true);
+  const handleCloseImageUpModal = () => setShowImageUpModal(false);
+  const handleShowChangePasswordModal = () => setShowChangePasswordModal(true);
+  const handleCloseChangePasswordModal = () => setShowChangePasswordModal(false);
+
   return <div>UserProfile</div>;
 };
 
