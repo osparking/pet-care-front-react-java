eff13db713064094619419e46f743525b804dc4a
osparking <jbpark03@gmail.com>
2024-12-29 12:56:21 +0900

    change password user service method

--- pet-care/src/components/user/UserService.js
+++ pet-care/src/components/user/UserService.js
@@ -17,3 +17,13 @@ export async function registUser(user) {
     throw err;
   }
 }
+
+export async function changePwd(userId, curPwd, newPwd, cnfPwd) {
+  try {
+    const request = { curPwd, newPwd, cnfPwd };
+    const result = await api.put(`/users/${userId}/change_pwd`, request);
+    return result.data;
+  } catch (err) {
+    throw err;
+  }
+}
