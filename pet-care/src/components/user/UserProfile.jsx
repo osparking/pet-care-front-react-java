15a7f259ea6b07110b03aae50aee7abbe33d61b8
osparking <jbpark03@gmail.com>
2024-12-29 17:53:31 +0900

    states for two modal sub-compos

--- pet-care/src/components/user/UserProfile.jsx
+++ pet-care/src/components/user/UserProfile.jsx
@@ -1,6 +1,9 @@
 import React from "react";
 
 const UserProfile = ({ user }) => {
+  const [showImageUpModal, setShowImageUpModal] = useState(false);
+  const [showChangePasswordModal, setShowChangePasswordModal] = useState(false);
+
   return <div>UserProfile</div>;
 };
 
