e7469736fcb310296293ddc25c45db94da3f41e0
osparking <jbpark03@gmail.com>
2024-12-30 20:18:43 +0900

    return value compo top 3 compos - skeleton

--- pet-care/src/components/user/UserDashboard.jsx
+++ pet-care/src/components/user/UserDashboard.jsx
@@ -6,7 +6,16 @@ import { useParams } from "react-router-dom";
 const UserDashboard = () => {
   const [user, setUser] = useState(null);
   const { userId } = useParams();
-  return (  );
+  return (
+    <Container>
+        <Tabs>
+            <Tab></Tab>
+            <Tab></Tab>
+            <Tab></Tab>
+            <Tab></Tab>
+        </Tabs>
+    </Container>
+  );
 };
 
 export default UserDashboard;
