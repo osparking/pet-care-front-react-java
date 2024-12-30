53638fba1698229e9d06dc31ed3005ec66c8daac
osparking <jbpark03@gmail.com>
2024-12-30 20:18:03 +0900

    user dashboard two state variables

---
+++ pet-care/src/components/user/UserDashboard.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Container } from "react-bootstrap";
+import { useParams } from "react-router-dom";
+
+
+const UserDashboard = () => {
+  const [user, setUser] = useState(null);
+  const { userId } = useParams();
+  return (  );
+};
+
+export default UserDashboard;
