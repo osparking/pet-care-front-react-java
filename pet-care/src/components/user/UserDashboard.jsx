f7442ffbd764fc9c482a27d799389e4ced340a6c
osparking <jbpark03@gmail.com>
2024-12-31 19:19:13 +0900

    import custom function, error msg temp change

--- pet-care/src/components/user/UserDashboard.jsx
+++ pet-care/src/components/user/UserDashboard.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import { Container, Tab, Tabs } from "react-bootstrap";
 import UseMsgAlerts from "../hooks/UseMsgAlerts";
 import UserProfile from "../user/UserProfile";
+import { getUserById } from "../user/UserService";
 const UserDashboard = () => {
   const [user, setUser] = useState(null);
   // const { userId } = useParams();
@@ -18,16 +19,16 @@ const UserDashboard = () => {
     setShowErrorAlert,
   } = UseMsgAlerts();
 
-
   useEffect(() => {
     const getUser = async () => {
       try {
-        console.log('user ID: ', userId);
+        console.log("user ID: ", userId);
         const result = await getUserById(userId);
         setUser(result.data);
-        console.log('user data: ', result.data);
+        console.log("user data: ", result.data);
       } catch (error) {
-        setErrorMsg(error.response.data.message);
+        setErrorMsg(error);
+        // setErrorMsg(error.response.data.message);
         setShowErrorAlert(true);
         console.error(error.message);
       }
