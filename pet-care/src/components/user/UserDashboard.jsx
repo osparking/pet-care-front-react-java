e7fe3aef2f3e7bc83c3851257435aed97c991c77
osparking <jbpark03@gmail.com>
2024-12-31 14:20:36 +0900

    read user data from backend

--- pet-care/src/components/user/UserDashboard.jsx
+++ pet-care/src/components/user/UserDashboard.jsx
@@ -2,20 +2,28 @@ import React from "react";
 import { Container } from "react-bootstrap";
 import { useParams } from "react-router-dom";
 
-
 const UserDashboard = () => {
   const [user, setUser] = useState(null);
   const { userId } = useParams();
+  useEffect(() => {
+    const getUser = async () => {
+      try {
+        const result = await getUserById(userId);
+        setUser(result.data);
+      } catch (error) {}
+    };
+  }, []);
+
   return (
     <Container>
-        <Tabs>
-            <Tab eventkey="profile" title={<h3>프로필</h3>}>
-                {user && (<UserProfile user={user}/>)}
-            </Tab>
-            <Tab></Tab>
-            <Tab></Tab>
-            <Tab></Tab>
-        </Tabs>
+      <Tabs>
+        <Tab eventkey="profile" title={<h3>프로필</h3>}>
+          {user && <UserProfile user={user} />}
+        </Tab>
+        <Tab></Tab>
+        <Tab></Tab>
+        <Tab></Tab>
+      </Tabs>
     </Container>
   );
 };
