552c86a4dcb4863ef61a0c223d6a4afb1b8cde06
osparking <jbpark03@gmail.com>
2025-01-02 15:44:57 +0900

    photo remove function in dashboard

--- pet-care/src/components/user/UserDashboard.jsx
+++ pet-care/src/components/user/UserDashboard.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { Container } from "react-bootstrap";
 import UseMsgAlerts from "../hooks/UseMsgAlerts";
+import { deleteUserPhoto } from "../modals/ImageService";
 import UserProfile from "../user/UserProfile";
 import { getUserById } from "../user/UserService";
 const UserDashboard = () => {
@@ -35,7 +36,28 @@ const UserDashboard = () => {
     getUser();
   }, [userId]);
 
-  return <Container>{user && <UserProfile user={user} />}</Container>;
+  const handleRemovePhoto = async () => {
+    console.log("삭제할 포토 소유 유저 ID: ", userId);
+    try {
+      const result = await deleteUserPhoto(userId);
+      window.location.reload();
+      setSuccessMsg(result.message);
+      setShowSuccessAlert(true);
+    } catch (e) {
+      console.log("error.response.data.message: ", error.response.data.message);
+      console.log("error.message: ", error.message);
+      setErrorMsg(error.response.data.message);
+      setShowErrorAlert(true);
+    }
+  };
+
+  return (
+    <Container>
+      {user && (
+        <UserProfile user={user} handleRemovePhoto={handleRemovePhoto} />
+      )}
+    </Container>
+  );
 };
 
 export default UserDashboard;
