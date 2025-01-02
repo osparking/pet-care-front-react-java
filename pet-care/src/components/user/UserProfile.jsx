20b2b6e71d3e4413f3d53920ffaeb3053629a485
osparking <jbpark03@gmail.com>
2025-01-02 21:10:27 +0900

    display account active status

--- pet-care/src/components/user/UserProfile.jsx
+++ pet-care/src/components/user/UserProfile.jsx
@@ -109,6 +109,17 @@ const UserProfile = ({ user, handleRemovePhoto }) => {
                 </Col>
               </Card.Body>
             )}
+
+            <Card.Body className="d-flex align-items-center">
+              <Col md={4}>계정 상태 :</Col>
+              <Col md={4}>
+                <Card.Text
+                  className={user.enabled ? styles.active : styles.inactive}
+                >
+                  {user.enabled ? "활성" : "비활성"}
+                </Card.Text>
+              </Col>
+            </Card.Body>
           </Card>
         </Col>
       </Row>
