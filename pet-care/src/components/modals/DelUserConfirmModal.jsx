3560853d081359a022df803d574407a17b098a16
osparking <jbpark03@gmail.com>
2025-01-04 12:51:12 +0900

    returning Modal compo' skeleton

--- pet-care/src/components/modals/DelUserConfirmModal.jsx
+++ pet-care/src/components/modals/DelUserConfirmModal.jsx
@@ -7,7 +7,13 @@ const DelUserConfirmModal = ({
   user,
   isLoading,
 }) => {
-  return <div>DelUserConfirmModal</div>;
+  return (
+    <Modal>
+      <Modal.Header>
+        <Modal.Title>계정 폐쇄 확인</Modal.Title>
+      </Modal.Header>
+    </Modal>
+  );
 };
 
 export default DelUserConfirmModal;
