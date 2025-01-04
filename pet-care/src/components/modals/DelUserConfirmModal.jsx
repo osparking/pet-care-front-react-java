3752c68e51c4fd504b25ba8b5a20cf9e9df81190
osparking <jbpark03@gmail.com>
2025-01-04 12:47:02 +0900

    modal confirms if user want to close account

---
+++ pet-care/src/components/modals/DelUserConfirmModal.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const DelUserConfirmModal = ({
+  show,
+  onHide,
+  handleDelete,
+  user,
+  isLoading,
+}) => {
+  return <div>DelUserConfirmModal</div>;
+};
+
+export default DelUserConfirmModal;
