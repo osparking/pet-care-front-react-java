c11bead09f206084c3b0b955534c4cc0a921ec86
osparking <jbpark03@gmail.com>
2025-01-04 13:00:07 +0900

    modal body confirms account owner's intention

--- pet-care/src/components/modals/DelUserConfirmModal.jsx
+++ pet-care/src/components/modals/DelUserConfirmModal.jsx
@@ -4,7 +4,7 @@ const DelUserConfirmModal = ({
   show,
   onHide,
   handleDelete,
-  user,
+  target,
   isLoading,
 }) => {
   return (
@@ -12,6 +12,9 @@ const DelUserConfirmModal = ({
       <Modal.Header>
         <Modal.Title>계정 폐쇄 확인</Modal.Title>
       </Modal.Header>
+      <Modal.Body>
+        이 {target}을/를 정말 삭제할 것입니까? 삭제 후 복구는 불가능합니다.
+      </Modal.Body>
     </Modal>
   );
 };
