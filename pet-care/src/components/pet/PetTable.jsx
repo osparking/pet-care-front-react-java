117c2f18f97adf761d402b3a01889406c8e06c44
osparking <jbpark03@gmail.com>
2025-01-06 22:35:29 +0900

    pet designated to be deleted

--- pet-care/src/components/pet/PetTable.jsx
+++ pet-care/src/components/pet/PetTable.jsx
@@ -3,6 +3,7 @@ import React from "react";
 const PetTable = ({ pets, apmtId, onPetUpdate, isEditable }) => {
   const [editModeId, setEditModeId] = useState(null);
   const [showDelModal, setShowDelModal] = useState(false);
+  const [petToDel, setPetToDel] = useState(null);
 
   return <div>PetTable</div>;
 };
