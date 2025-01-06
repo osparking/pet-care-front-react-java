e7211137c660d5d2c75cd128788b988512701809
osparking <jbpark03@gmail.com>
2025-01-06 21:48:44 +0900

    editPet state and petChange callback ftn

--- pet-care/src/components/pet/EditablePetRow.jsx
+++ pet-care/src/components/pet/EditablePetRow.jsx
@@ -1,9 +1,14 @@
-import React from 'react'
+import React from "react";
 
-const EditablePetRow = ({ pet, index, onSave, onCancel}) => {
-  return (
-    <div>EditablePetRow</div>
-  )
-}
+const EditablePetRow = ({ pet, index, onSave, onCancel }) => {
+  const [editPet, setEditPet] = useState(pet);
+  const handleChange = (e) => {
+    setEditPet((currPet) => ({
+      ...currPet,
+      [e.target.name]: e.target.value,
+    }));
+  };
+  return <div>EditablePetRow</div>;
+};
 
-export default EditablePetRow
+export default EditablePetRow;
