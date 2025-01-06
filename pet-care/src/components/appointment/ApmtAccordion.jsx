0a667c26dc1f1c5e095a377ff9f12c1a3f5b0913
osparking <jbpark03@gmail.com>
2025-01-06 09:13:55 +0900

    treatment appoint summary accordion skeleton

--- pet-care/src/components/appointment/ApmtAccordion.jsx
+++ pet-care/src/components/appointment/ApmtAccordion.jsx
@@ -1,7 +1,19 @@
 import React from "react";
+import { Accordion, Container } from "react-bootstrap";
 
 const ApmtAccordion = ({ apmts }) => {
-  return <div>ApmtAccordion</div>;
+  return (
+    <Container>
+      <Accordion>
+        {apmts.map((apmt) => (
+          <Accordion.Item key={apmt.id}>
+            <Accordion.Header>{apmt.date}</Accordion.Header>
+            <Accordion.Body>{apmt.reason}</Accordion.Body>
+          </Accordion.Item>
+        ))}
+      </Accordion>
+    </Container>
+  );
 };
 
 export default ApmtAccordion;
