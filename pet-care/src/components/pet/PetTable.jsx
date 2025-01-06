5f2625403463819050c17b36b75572d8ae78e0d5
osparking <jbpark03@gmail.com>
2025-01-06 23:05:54 +0900

    function that handles pet edit or cancel

--- pet-care/src/components/pet/PetTable.jsx
+++ pet-care/src/components/pet/PetTable.jsx
@@ -1,9 +1,28 @@
 import React from "react";
+import UseMsgAlerts from "../hooks/UseMsgAlerts";
 
 const PetTable = ({ pets, apmtId, onPetUpdate, isEditable }) => {
   const [editModeId, setEditModeId] = useState(null);
   const [showDelModal, setShowDelModal] = useState(false);
   const [petToDel, setPetToDel] = useState(null);
+  const {
+    successMsg,
+    setSuccessMsg,
+    errorMsg,
+    setErrorMsg,
+    showSuccessAlert,
+    setShowSuccessAlert,
+    showErrorAlert,
+    setShowErrorAlert,
+  } = UseMsgAlerts;
+
+  const handleEdit = (petId) => {
+    setEditModeId(petId);
+  };
+
+  const handleCancel = () => {
+    setEditModeId(null);
+  };
 
   return <div>PetTable</div>;
 };
