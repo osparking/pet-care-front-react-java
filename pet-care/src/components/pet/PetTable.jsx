d2e46d200a1f48932a71ac747921d8168ec5e80d
osparking <jbpark03@gmail.com>
2025-01-06 22:28:44 +0900

    id points the pet row being edited

--- pet-care/src/components/pet/PetTable.jsx
+++ pet-care/src/components/pet/PetTable.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 
 const PetTable = ({ pets, apmtId, onPetUpdate, isEditable }) => {
+  const [editModeId, setEditModeId] = useState(null);
+
   return <div>PetTable</div>;
 };
 
