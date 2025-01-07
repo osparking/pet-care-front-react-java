117d7c05577d47f40894b7aabf9e72aa91319852
osparking <jbpark03@gmail.com>
2025-01-07 21:29:16 +0900

    color assign for all appoint status

--- pet-care/src/components/hooks/ColorMapping.js
+++ pet-care/src/components/hooks/ColorMapping.js
@@ -7,6 +7,16 @@ const useColorMapping = () => {
     const rootStyle = getComputedStyle(document.documentElement);
     setColors({
       "진료 중": rootStyle.getPropertyValue("--color-on-going"),
+      "임박함": rootStyle.getPropertyValue("--color-up-coming"),
+      "완료됨": rootStyle.getPropertyValue("--color-completed"),
+      "거부됨": rootStyle.getPropertyValue("--color-not-approved"),
+      "취소됨": rootStyle.getPropertyValue("--color-cancelled"),
+      "승인대기": rootStyle.getPropertyValue(
+        "--color-waiting-for-approval"
+      ),
+      "보류 중": rootStyle.getPropertyValue("--color-pending"),
+      "승인됨": rootStyle.getPropertyValue("--color-approved"),
+      default: rootStyle.getPropertyValue("--color-default"),
     });
   }, []);
 
