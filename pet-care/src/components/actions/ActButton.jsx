98cce4f0d2035efd7df78ac006d8d4484d825f8b
osparking <jbpark03@gmail.com>
2025-01-08 09:19:24 +0900

    general action button skeleton

---
+++ pet-care/src/components/actions/ActButton.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const ActButton = ({
+  title,
+  variant,
+  onClick,
+  disabled,
+  isProcessing,
+  className = "",
+}) => {
+  return <div>ActButton</div>;
+};
+
+export default ActButton;
