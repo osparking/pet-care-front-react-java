67f96b49f6647f52b7b89d61de228a4eb86bd233
osparking <jbpark03@gmail.com>
2025-01-08 17:46:12 +0900

    update form submit handler function

--- pet-care/src/components/modals/ApmtUpdateModal.jsx
+++ pet-care/src/components/modals/ApmtUpdateModal.jsx
@@ -5,6 +5,17 @@ const ApmtUpdateModal = ({ show, apmt, doClose, doUpdate }) => {
   const [apmtTime, setApmtTime] = useState(
     new Date(`${apmt.date}T${apmt.time}`)
   );
+  const [reason, setReason] = useState(apmt.reason);
+
+  const handleSubmit = () => {
+    const updatedApmt = {
+      ...apmt,
+      date: apmtDate.toISOString().split("T")[0],
+      time: apmtTime.toTimeString().split(" ")[0].substring(0, 5),
+      reason,
+    };
+    doUpdate(updatedApmt);
+  };
 
   return <div>ApmtUpdateModal</div>;
 };
