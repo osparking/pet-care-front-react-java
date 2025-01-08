3fb1abb9302a279314e111802c2244fdc77f49fe
osparking <jbpark03@gmail.com>
2025-01-08 15:41:20 +0900

    declare appoint date&time state vari

--- pet-care/src/components/modals/ApmtUpdateModal.jsx
+++ pet-care/src/components/modals/ApmtUpdateModal.jsx
@@ -1,6 +1,11 @@
 import React from "react";
 
 const ApmtUpdateModal = ({ show, apmt, doClose, doUpdate }) => {
+  const [apmtDate, setApmtDate] = useState(new Date(apmt.date));
+  const [apmtTime, setApmtTime] = useState(
+    new Date(`${apmt.date}T${apmt.time}`)
+  );
+
   return <div>ApmtUpdateModal</div>;
 };
 
