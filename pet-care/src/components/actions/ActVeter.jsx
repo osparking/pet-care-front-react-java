85b0ad9134934f4fa07c4e9458a5e2d73fe66df1
osparking <jbpark03@gmail.com>
2025-01-08 12:36:28 +0900

    appointment vet action processing function

--- pet-care/src/components/actions/ActVeter.jsx
+++ pet-care/src/components/actions/ActVeter.jsx
@@ -3,6 +3,29 @@ import ActButton from "./ActButton";
 
 const ActVeter = ({ onApprove, onDecline, disabled }) => {
   const [isProcessing, setIsProcessing] = useState(false);
+
+  const handleClick = (actionType) => {
+    setIsProcessing(true);
+    // actionType 에 따라 다르게 처리한다.
+    if (actionType === "approve") {
+      onApprove()
+        .then(() => {
+          setIsProcessing(false);
+        })
+        .catch(() => {
+          setIsProcessing(false);
+        });
+    } else {
+      onDecline()
+        .then(() => {
+          setIsProcessing(false);
+        })
+        .catch(() => {
+          setIsProcessing(false);
+        });
+    }
+  };
+
   return (
     <section>
       <ActButton />
