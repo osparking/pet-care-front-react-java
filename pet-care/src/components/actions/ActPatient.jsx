54cda56bf660f80190a9ba93160758c90ba31b82
osparking <jbpark03@gmail.com>
2025-01-08 10:00:59 +0900

    processing state, button click processing

--- pet-care/src/components/actions/ActPatient.jsx
+++ pet-care/src/components/actions/ActPatient.jsx
@@ -1,6 +1,11 @@
 import React from "react";
 
 const ActPatient = ({ onUpdate, onCancel, disabled }) => {
+  const [isProcessing, setIsProcessing] = useState(false);
+  const handleClick = (actionType) => {
+    setIsProcessing(true);
+    // actionType 에 따라 다르게 처리한다.
+  };
   return <div>ActPatient</div>;
 };
 
