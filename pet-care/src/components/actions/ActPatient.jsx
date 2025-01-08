8a5bfc4fb9fb4792b4cda7db8d1e011ad4652820
osparking <jbpark03@gmail.com>
2025-01-08 10:47:13 +0900

    use callback for each action type

--- pet-care/src/components/actions/ActPatient.jsx
+++ pet-care/src/components/actions/ActPatient.jsx
@@ -5,6 +5,23 @@ const ActPatient = ({ onUpdate, onCancel, disabled }) => {
   const handleClick = (actionType) => {
     setIsProcessing(true);
     // actionType 에 따라 다르게 처리한다.
+    if (actionType === "update") {
+      onUpdate()
+        .then(() => {
+          setIsProcessing(false);
+        })
+        .catch(() => {
+          setIsProcessing(false);
+        });
+    } else {
+      onCancel()
+        .then(() => {
+          setIsProcessing(false);
+        })
+        .catch(() => {
+          setIsProcessing(false);
+        });
+    }
   };
   return <div>ActPatient</div>;
 };
