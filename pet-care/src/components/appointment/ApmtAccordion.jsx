1ae4b9034a5c36e6fd8f4490555eaf70b5f6594a
osparking <jbpark03@gmail.com>
2025-01-09 11:19:03 +0900

    appointment update API function - api.put()

--- pet-care/src/components/appointment/ApmtAccordion.jsx
+++ pet-care/src/components/appointment/ApmtAccordion.jsx
@@ -8,6 +8,7 @@ import useColorMapping from "../hooks/ColorMapping";
 import UseMsgAlerts from "../hooks/UseMsgAlerts";
 import PetTable from "../pet/PetTable";
 import { UserType } from "../utils/utilities";
+import { updateApmt } from "./ServiceAppointment";
 
 const ApmtAccordion = ({ user, apmts: oldApmts, isPatient }) => {
   registerLocale("ko", ko);
--- pet-care/src/components/appointment/ServiceAppointment.js
+++ pet-care/src/components/appointment/ServiceAppointment.js
@@ -11,3 +11,13 @@ export async function bookAppointment(senderId, recipientId, request) {
     throw err;
   }
 }
+
+export const updateApmt = async function updateApmt(apmtId, apmt) {
+  try {
+    const result = await api.put(`/appointments//${apmtId}/update`, apmt);
+    console.log("예약 갱신 - api 호출 결과 메시지: ", result.data.message);
+    return result;
+  } catch (err) {
+    throw err;
+  }
+};
