3c620b6b9612d889c4bb556e737d157c25591239
osparking <jbpark03@gmail.com>
2025-01-09 12:40:20 +0900

    remove extra slash from appoint update url

--- pet-care/src/components/appointment/ServiceAppointment.js
+++ pet-care/src/components/appointment/ServiceAppointment.js
@@ -14,7 +14,7 @@ export async function bookAppointment(senderId, recipientId, request) {
 
 export const updateApmt = async function updateApmt(apmtId, apmt) {
   try {
-    const result = await api.put(`/appointments//${apmtId}/update`, apmt);
+    const result = await api.put(`/appointments/${apmtId}/update`, apmt);
     console.log("예약 갱신 - api 호출 결과 메시지: ", result.data.message);
     return result;
   } catch (err) {
