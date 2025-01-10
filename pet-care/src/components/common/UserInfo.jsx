051169af999d8c038bf609208ad432681df6ab92
osparking <jbpark03@gmail.com>
2025-01-10 20:37:50 +0900

    style return div and title

--- pet-care/src/components/common/UserInfo.jsx
+++ pet-care/src/components/common/UserInfo.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 const UserInfo = ({ userType, apmt }) => {
-  return <div>UserInfo</div>;
+  return (
+    <div className="mt-2 mb-2" style={{ backgroundColor: "whitesmoke" }}>
+      <h5>{userType === "VET" ? "팻 주인" : "수의사"} 인적 사항</h5>
+    </div>
+  );
 };
 
 export default UserInfo;
