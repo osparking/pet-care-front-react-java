755623c7225fe7af802db6c12dad7185986378f3
osparking <jbpark03@gmail.com>
2025-01-11 11:11:12 +0900

    appointment filter -skeleton

---
+++ pet-care/src/components/appointment/FilterAppointment.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const FilterAppointment = ({
+  statuses = [],
+  selectedStat,
+  onSelectStat,
+  onClearFilter,
+}) => {
+  return <div>FilterAppointment</div>;
+};
+
+export default FilterAppointment;
