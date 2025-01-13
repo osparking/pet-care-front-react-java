61a682dbda08333c34c716cd2fc35ff03a2ad7c4
osparking <jbpark03@gmail.com>
2025-01-13 11:14:09 +0900

    customized pie chart - skeleton

---
+++ pet-care/src/components/charts/CustomPie.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const CustomPie = (
+  data,
+  dataKey = "value",
+  nameKey = "name",
+  width = "80%",
+  height = 400
+) => {
+  return <div>CustomPie</div>;
+};
+
+export default CustomPie;
