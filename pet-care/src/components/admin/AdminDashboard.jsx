eb00de075b2bcbceb5d766381a73046b374a32ef
osparking <jbpark03@gmail.com>
2025-01-16 17:48:55 +0900

    admin dashboar has Overview compo

---
+++ pet-care/src/components/admin/AdminDashboard.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Overview from "./overview";
+
+const AdminDashboard = () => {
+  return (
+    <div>
+      <Overview />
+    </div>
+  );
+};
+
+export default AdminDashboard;
