517e8afcc254af1748ad82344e638c8c0c6df103
osparking <jbpark03@gmail.com>
2025-01-16 14:20:51 +0900

    three states for admin overview compo

---
+++ pet-care/src/components/appointment/admin/overview.jsx
@@ -0,0 +1,11 @@
+import React, { useEffect } from "react";
+
+const Overview = () => {
+  const [userCount, setUserCount] = useState(0);
+  const [vetCount, setVetCount] = useState(0);
+  const [patientCount, setPatientCount] = useState(0);
+
+  return <div>overview</div>;
+};
+
+export default Overview;
