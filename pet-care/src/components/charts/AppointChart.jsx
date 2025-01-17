0e093f8eda1ab9cfe28f2369f7f20837646d815a
osparking <jbpark03@gmail.com>
2025-01-18 08:09:45 +0900

    appointment chart compo - skeleton

---
+++ pet-care/src/components/charts/AppointChart.jsx
@@ -0,0 +1,9 @@
+import React, { useState } from "react";
+
+const AppointChart = () => {
+  const [appointData, setAppointData] = useState([]);
+  const [errorMessage, setErrorMessage] = useState("");
+  return <div>AppointChart</div>;
+};
+
+export default AppointChart;
