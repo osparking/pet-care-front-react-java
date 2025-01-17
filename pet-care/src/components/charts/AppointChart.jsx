b5693fd0c32e8c7ed757159ef7cb107fa9adfc52
osparking <jbpark03@gmail.com>
2025-01-18 08:52:07 +0900

    at load, appoint chart data is read

--- pet-care/src/components/charts/AppointChart.jsx
+++ pet-care/src/components/charts/AppointChart.jsx
@@ -1,8 +1,22 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 const AppointChart = () => {
   const [appointData, setAppointData] = useState([]);
   const [errorMessage, setErrorMessage] = useState("");
+
+  useEffect(() => {
+    const readAppointmentData = async () => {
+      try {
+        // Fetch appointments data calling service function
+        const response = await getAppointData();
+        setAppointData(response.data);
+      } catch (error) {
+        setErrorMessage(error.message);
+      }
+    };
+    readAppointmentData();
+  }, []);
+
   return <div>AppointChart</div>;
 };
 
