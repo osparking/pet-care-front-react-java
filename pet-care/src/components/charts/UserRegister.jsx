b99af53bd2112cee2bb77295c6502fb0d22b5421
osparking <jbpark03@gmail.com>
2025-01-17 17:34:34 +0900

    hook reads user register stat thru api

--- pet-care/src/components/charts/UserRegister.jsx
+++ pet-care/src/components/charts/UserRegister.jsx
@@ -1,8 +1,22 @@
 import React, { useState } from "react";
+import { getUserByMonthType } from "../user/UserService";
 
 const UserRegister = () => {
   const [userStat, setUserStat] = useState([]);
   const [errorMessage, setErrorMessage] = useState(null);
+
+  useEffect(() => {
+    const getUserStat = async () => {
+      try {
+        const response = await getUserByMonthType();
+        const userStat = await response.data;
+        console.log("user stat:", userStat);
+      } catch (err) {
+        setErrorMessage(err.message)
+      }
+    };
+    getUserStat();
+  }, []);
 
   return <div>UserRegister</div>;
 };
