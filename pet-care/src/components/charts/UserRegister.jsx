7e1474bad5daab7e3eae76f39b668563b0055aa0
osparking <jbpark03@gmail.com>
2025-01-17 17:22:57 +0900

    user register statistics chart component

---
+++ pet-care/src/components/charts/UserRegister.jsx
@@ -0,0 +1,10 @@
+import React, { useState } from "react";
+
+const UserRegister = () => {
+  const [userStat, setUserStat] = useState([]);
+  const [errorMessage, setErrorMessage] = useState(null);
+
+  return <div>UserRegister</div>;
+};
+
+export default UserRegister;
