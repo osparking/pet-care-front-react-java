37c05b7f667da996c3f4b1858a881c25a5f5bd73
osparking <jbpark03@gmail.com>
2025-01-18 18:22:26 +0900

    for each element, create one object

--- pet-care/src/components/charts/AccountActive.jsx
+++ pet-care/src/components/charts/AccountActive.jsx
@@ -6,16 +6,26 @@ const AccountActive = () => {
   const [errorMessage, setErrorMessage] = useState("");
 
   useEffect(() => {
-    const getAcccountTypeActStat  = async () => {
-        try {
-          const result = await getUserActivenessStat();
-          const userActivenessStat = result.data;
+    const getAcccountTypeActStat = async () => {
+      try {
+        const result = await getUserActivenessStat();
+        const userActivenessStat = result.data;
 
-        } catch (error) {
-          setErrorMessage(error.message);
-        }
-      };
-      getAcccountTypeActStat();      
+        // 후단 근본 자료를 차트용 자료로 변형
+        const accountData = Object.entries(userActivenessStat).flatMap(
+          ([status, counts]) => [
+            {
+              name: "활성인 팻주인",
+              value: status === "Enabled" ? counts.PATIENT : 0,
+              color: "#d26161",
+            },
+          ]
+        );
+      } catch (error) {
+        setErrorMessage(error.message);
+      }
+    };
+    getAcccountTypeActStat();
   }, []);
 
   return <div>AccountActive</div>;
