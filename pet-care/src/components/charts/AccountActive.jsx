071bae33e8792b848077c0ff9147e99ac4b8c7a3
osparking <jbpark03@gmail.com>
2025-01-18 15:22:56 +0900

    user activeness statictics - just skeleton

---
+++ pet-care/src/components/charts/AccountActive.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const AccountActive = () => {
+  const [accountData, setAccountData] = useState([]);
+  const [errorMessage, setErrorMessage] = useState("");
+  return <div>AccountActive</div>;
+};
+
+export default AccountActive;
