7c97b472066454d256275070a45de48f74941441
osparking <jbpark03@gmail.com>
2025-01-19 09:15:55 +0900

    vet specialty count chart - skeleton

---
+++ pet-care/src/components/charts/VetSpecChart.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+const VetSpecChart = () => {
+  const [vetSpecials, setVetSpecials] = useState([]);
+  const [errorMessage, setErrorMessage] = useState("");
+
+  return <div></div>;
+};
+
+export default VetSpecChart;
