745128ec8f6346fd6069b35a1276a840e88b509a
osparking <jbpark03@gmail.com>
2025-01-20 15:37:21 +0900

    remove error causing js code

--- pet-care/src/components/utils/utilities.js
+++ pet-care/src/components/utils/utilities.js
@@ -29,24 +29,3 @@ export const UserType = {
   PATIENT: "PATIENT",
   VET: "VET",
 };
-
-export const generateColor = (specName) => {
-  // 기본 값 부여
-  if (typeof specName !== "string" || specName.length === 0) {
-    return "#8884d8"; // 군청색
-  }
-
-  // 해쉬 코드 생성 - 전문분야 문자열을 근거로 함
-  let hash = 0;
-  for (let i = 0; i < hash.length; i++) {
-    const char = specName.charCodeAt(i);
-    hash = (hash << 5) - hash + char;
-    hash = hash & hash; // Convert to 32bit integer
-  }
-
-  // 0 ~ 359 범위 중 하나의 값을 hue 로 사용
-  // saturation: 70, lightness: 50 - HSL 축약어.
-  const hue = hash % 360;
-  return `hsl(${hue}, 70%, 50%)`;
-
-}
