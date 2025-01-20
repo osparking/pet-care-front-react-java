59c5fa49f6db25ab11126be9b9c77733e772ae4b
osparking <jbpark03@gmail.com>
2025-01-20 14:51:45 +0900

    correct chart data field name to specialty

--- pet-care/src/components/charts/VetSpecChart.jsx
+++ pet-care/src/components/charts/VetSpecChart.jsx
@@ -25,7 +25,7 @@ const VetSpecChart = () => {
         console.log("전문분야: ", response.data);
         const specialsWithColor = rawSpecials.map((special) => ({
           ...special,
-          color: generateColor(special.name),
+          color: generateColor(special.specialty),
         }));
         setVetSpecials(specialsWithColor);
       } catch (error) {
@@ -37,7 +37,7 @@ const VetSpecChart = () => {
   }, []);
 
   return (
-    <ResponsiveContainer width={"80%"} height={400}>
+    <ResponsiveContainer width={"60%"} height={400}>
       <h5 className="mt-4 mb-4 chart-title">수의사 분야 통계</h5>
       <BarChart data={vetSpecials}>
         <XAxis dataKey="speciality" angle={-30} textAnchor="end" height={70} />
