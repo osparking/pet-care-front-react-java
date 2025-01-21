9022159de8bd0fbe5d5b0b64dd7d94fd822b411a
osparking <jbpark03@gmail.com>
2025-01-21 09:12:34 +0900

    admin sidebar - skeleton

---
+++ pet-care/src/components/admin/AdminSideBar.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const AdminSideBar = ({
+  openSidebarToggle,
+  OpenSidebar,
+  onNavigate,
+  activeTab,
+}) => {
+  return <div></div>;
+};
+
+export default AdminSideBar;
