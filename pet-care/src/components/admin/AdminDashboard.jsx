43d11069251cc3d797f2519d3d5f54d5e36b6f65
osparking <jbpark03@gmail.com>
2025-01-21 11:36:49 +0900

    side bar toggler, active tab selector

--- pet-care/src/components/admin/AdminDashboard.jsx
+++ pet-care/src/components/admin/AdminDashboard.jsx
@@ -5,6 +5,14 @@ const AdminDashboard = () => {
   const [openSidebar, setOpenSidebar] = useState(false);
   const [activeTab, setActiveTab] = useState("overview");
 
+  const toggleSidebar = () => {
+    setOpenSidebar(!openSidebar);
+  };
+
+  const handleTabClick = (tab) => {
+    setActiveTab(tab);
+  };
+
   return (
     <div>
       <Overview />
