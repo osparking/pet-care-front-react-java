e76a65ee0151358c4816d9d5142832b3bb72fd41
osparking <jbpark03@gmail.com>
2025-01-21 15:49:00 +0900

    Veterin compo skeleton

---
+++ pet-care/src/components/admin/veterin.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Veterin = () => {
+  const [veterins, setVetermins] = useState([]);
+  const [showDelModal, setShowDelModal] = useState(false);
+  const [vetToDel, setVetToDel] = useState(null);
+
+  return <div></div>;
+};
+
+export default Veterin;
