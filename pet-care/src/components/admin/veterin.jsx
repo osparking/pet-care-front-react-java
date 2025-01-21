71a2e4cde27cfe190c44530cbefe034f9bc1c452
osparking <jbpark03@gmail.com>
2025-01-21 15:54:33 +0900

    bring in messages using custom hook

--- pet-care/src/components/admin/veterin.jsx
+++ pet-care/src/components/admin/veterin.jsx
@@ -1,9 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
+import UseMsgAlerts from "../hooks/UseMsgAlerts";
 
 const Veterin = () => {
   const [veterins, setVetermins] = useState([]);
   const [showDelModal, setShowDelModal] = useState(false);
   const [vetToDel, setVetToDel] = useState(null);
+
+  const {
+    successMsg,
+    setSuccessMsg,
+    errorMsg,
+    setErrorMsg,
+    showSuccessAlert,
+    setShowSuccessAlert,
+    showErrorAlert,
+    setShowErrorAlert,
+  } = UseMsgAlerts();
 
   return <div></div>;
 };
