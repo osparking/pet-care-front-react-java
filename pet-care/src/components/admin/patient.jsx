3c1a6f9be8c4f0d19a3a93da72875af001ae3c50
osparking <jbpark03@gmail.com>
2025-01-22 21:42:24 +0900

    repeat unique tbody-tr for each patient

--- pet-care/src/components/admin/patient.jsx
+++ pet-care/src/components/admin/patient.jsx
@@ -2,8 +2,8 @@ import React, { useEffect, useState } from "react";
 import { Col, OverlayTrigger, Row, Table, Tooltip } from "react-bootstrap";
 import { BsEyeFill } from "react-icons/bs";
 import { Link } from "react-router-dom";
-import UseMsgAlerts from "../hooks/UseMsgAlerts";
 import AlertMessage from "../common/AlertMessage";
+import UseMsgAlerts from "../hooks/UseMsgAlerts";
 
 const Patient = () => {
   const [patients, setPatients] = useState([]);
@@ -60,29 +60,31 @@ const Patient = () => {
           </tr>
         </thread>
         <tbody>
-          <tr>
-            <td>{patient.id}</td>
-            <td>{patient.lastName}</td>
-            <td>{patient.firstName}</td>
-            <td>{patient.email}</td>
-            <td>{patient.mobile}</td>
-            <td>{patient.gender}</td>
-            <td>{patient.createdAt}</td>
-            <td>
-              <OverlayTrigger
-                overlay={
-                  <Tooltip id={`tooltip-view-${index}`}>팻 주인 열람</Tooltip>
-                }
-              >
-                <Link
-                  to={`/user_dashboard/${patient.id}/my_dashboard`}
-                  className="text-info"
+          {patients.map((patient, idx) => (
+            <tr key={idx}>
+              <td>{patient.id}</td>
+              <td>{patient.lastName}</td>
+              <td>{patient.firstName}</td>
+              <td>{patient.email}</td>
+              <td>{patient.mobile}</td>
+              <td>{patient.gender}</td>
+              <td>{patient.createdAt}</td>
+              <td>
+                <OverlayTrigger
+                  overlay={
+                    <Tooltip id={`tooltip-view-${index}`}>팻 주인 열람</Tooltip>
+                  }
                 >
-                  <BsEyeFill />
-                </Link>
-              </OverlayTrigger>
-            </td>
-          </tr>
+                  <Link
+                    to={`/user_dashboard/${patient.id}/my_dashboard`}
+                    className="text-info"
+                  >
+                    <BsEyeFill />
+                  </Link>
+                </OverlayTrigger>
+              </td>
+            </tr>
+          ))}
         </tbody>
       </Table>
     </main>
