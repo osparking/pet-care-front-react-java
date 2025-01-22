f5a146041d80f77bed3b084ebab1c60ee5613f12
osparking <jbpark03@gmail.com>
2025-01-22 14:42:37 +0900

    vet selection filter - skeleton

---
+++ pet-care/src/components/veterinarians/VetFilter.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const VetFilter = ({
+  specials = [],
+  selectedSpecial,
+  onSelectSpecial,
+  onClearFilter,
+}) => {
+  return <div></div>;
+};
+
+export default VetFilter;
