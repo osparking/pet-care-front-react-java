8c1a0bb5682156f497c358e8fe19ff0f0a28b5e3
osparking <jbpark03@gmail.com>
2025-01-22 14:22:47 +0900

    tooptip compo source changed

--- pet-care/src/components/admin/veterin.jsx
+++ pet-care/src/components/admin/veterin.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { Col, OverlayTrigger, Row, Table } from "react-bootstrap";
+import { Col, OverlayTrigger, Row, Table, Tooltip } from "react-bootstrap";
 import {
   BsEyeFill,
   BsLockFill,
@@ -9,7 +9,6 @@ import {
   BsUnlockFill,
 } from "react-icons/bs";
 import { Link } from "react-router-dom";
-import { Tooltip } from "recharts";
 import AlertMessage from "../common/AlertMessage";
 import UseMsgAlerts from "../hooks/UseMsgAlerts";
 import DelTargetConfirmModal from "../modals/DelTargetConfirmModal";
