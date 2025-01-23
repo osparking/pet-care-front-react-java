0c0e5e9606535902e22f3972f96579a1f5d6eb5f
osparking <jbpark03@gmail.com>
2025-01-23 13:09:51 +0900

    makes axios not to throw

--- pet-care/src/components/patient/PatientService.js
+++ pet-care/src/components/patient/PatientService.js
@@ -2,7 +2,12 @@ import { api } from "../utils/api";
 
 export async function getPatients() {
   try {
-    const response = await api.get("/users/get_all_patients");
+    const response = await api.get("/users/get_all_patients", {
+      validateStatus: function (status) {
+        return true;
+      },
+    });
+    console.log("반응자료:", response.data);
     return response.data;
   } catch (err) {
     throw err;
