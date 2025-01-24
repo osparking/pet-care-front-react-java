f792689c536d88bdfc8e2f9a74e9902e67771211
osparking <jbpark03@gmail.com>
2025-01-24 14:16:19 +0900

    save selected tab in local storage

--- pet-care/src/components/admin/AdminDashboard.jsx
+++ pet-care/src/components/admin/AdminDashboard.jsx
@@ -1,8 +1,8 @@
 import React, { useState } from "react";
 import AdminSideBar from "./AdminSideBar";
 import Overview from "./Overview";
-import Veterin from "./veterin";
 import Patient from "./patient";
+import Veterin from "./veterin";
 
 const AdminDashboard = () => {
   const [openSidebar, setOpenSidebar] = useState(false);
@@ -14,6 +14,8 @@ const AdminDashboard = () => {
 
   const handleTabClick = (tab) => {
     setActiveTab(tab);
+    localStorage.setItem("activeTab", tab);
+    console.log("activeTab: " + tab);
   };
 
   return (
