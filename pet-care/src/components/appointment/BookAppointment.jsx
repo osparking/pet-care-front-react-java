54bf4cc4195772b14191ff2ca9bf8aa8823c7d00
osparking <jbpark03@gmail.com>
2025-02-04 15:29:58 +0900

    read senderId from the path string

--- pet-care/src/components/appointment/BookAppointment.jsx
+++ pet-care/src/components/appointment/BookAppointment.jsx
@@ -40,8 +40,8 @@ const BookAppointment = () => {
     ],
   });
 
-  const senderId = 2062;
-  const { recipientId } = useParams();
+  // const senderId = 2062;
+  const { recipientId, senderId } = useParams();
 
   const dateChanged = (newDate) => {
     setFormData((prevState) => ({ ...prevState, date: newDate }));
