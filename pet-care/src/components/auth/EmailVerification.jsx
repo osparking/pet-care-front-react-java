98e56d91d89bfa9fc5ae4710f5a6a554ec73ed98
osparking <jbpark03@gmail.com>
2025-02-05 17:34:12 +0900

    process repeated email verification trial

--- pet-care/src/components/auth/EmailVerification.jsx
+++ pet-care/src/components/auth/EmailVerification.jsx
@@ -15,11 +15,14 @@ const EmailVerification = () => {
           setVerifyMsg("이메일 검증이 성공하여 로그인이 가능합니다.");
           setAlertType("alert-success");
           break;
+        case "검증된 토큰":
+          setVerifyMsg("이메일 검증이 이미 완료된 바 있습니다.");
+          setAlertType("alert-info");
+          break;
         default:
           break;
       }
       /*
-        VERIFIED_TOKEN = "검증된 토큰";
         TOKEN_VALIDATED = "계정 활성화됨";
         {TOKEN_VALI_ERROR = "토큰 검증 오류";
         TOKEN_EXPIRED = "토큰 기한 만료";
