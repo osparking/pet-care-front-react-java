720241aff139dc5b894e9d753280918c1d1c4d53
osparking <jbpark03@gmail.com>
2025-02-06 17:48:51 +0900

    fix Alert property name typo

--- pet-care/src/components/common/AlertMessage.jsx
+++ pet-care/src/components/common/AlertMessage.jsx
@@ -3,8 +3,9 @@ import { Alert } from "react-bootstrap";
 
 export default function AlertMessage({ type, message }) {
   if (!message) return null;
+  console.log("type: " + type + ", message: " + message);
   return (
-    <Alert varient={type} dismissible>
+    <Alert variant={type} dismissible>
       {message}
     </Alert>
   );
