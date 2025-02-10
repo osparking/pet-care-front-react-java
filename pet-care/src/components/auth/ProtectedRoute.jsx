3c6e99d0c2677f89e202f06e17f7ff2298cac0ee
osparking <jbpark03@gmail.com>
2025-02-10 20:33:14 +0900

    decides if user is autho'ed for the route

--- pet-care/src/components/auth/ProtectedRoute.jsx
+++ pet-care/src/components/auth/ProtectedRoute.jsx
@@ -1,14 +1,16 @@
 import React from "react";
 import { useLocation } from "react-router-dom";
 
-const ProtectedRoute = ({
-  children,
-  allowedRoutes = [],
-  useOutlet = false,
-}) => {
+const ProtectedRoute = ({ children, allowedRoles = [], useOutlet = false }) => {
   const loggedIn = localStorage.getItem("authToken");
   const userRoles = JSON.parse(localStorage.getItem("userRoles")) || [];
   const location = useLocation();
+
+  const userRolesLower = userRoles.map((role) => role.toLowerCase());
+  const allowedRolesLower = allowedRoles.map((role) => role.toLowerCase());
+  const isAuthorized = userRolesLower.some((uRole) =>
+    allowedRolesLower.includes(uRole)
+  );
 
   if (!loggedIn) {
     // login 페이지로 보내고, *로그인 후 복귀할 수 있게) 직전 위치 기억
