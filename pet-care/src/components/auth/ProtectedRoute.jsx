fc7a43cced0e790c939da8ada2cb909d9260eed9
osparking <jbpark03@gmail.com>
2025-02-10 17:47:53 +0900

    protect routes using roles - skeleton

---
+++ pet-care/src/components/auth/ProtectedRoute.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const ProtectedRoute = ({
+  children,
+  allowedRoutes = [],
+  useOutlet = false,
+}) => {
+  return <div></div>;
+};
+
+export default ProtectedRoute;
