0789750e9c8819e411fdec3b4f7633badeaa7d1e
osparking <jbpark03@gmail.com>
2025-02-10 17:51:55 +0900

    some consts needed to protect routes

--- pet-care/src/components/auth/ProtectedRoute.jsx
+++ pet-care/src/components/auth/ProtectedRoute.jsx
@@ -1,10 +1,15 @@
 import React from "react";
+import { useLocation } from "react-router-dom";
 
 const ProtectedRoute = ({
   children,
   allowedRoutes = [],
   useOutlet = false,
 }) => {
+  const authToken = localStorage.getItem("authToken");
+  const userRoles = JSON.parse(localStorage.getItem("userRoles")) || [];
+  const location = useLocation();
+  
   return <div></div>;
 };
 
