eb6451b5c707a984f8c005a0e92ebb6721010cae
osparking <jbpark03@gmail.com>
2025-02-11 09:28:35 +0900

    fix  'Outlet is not defined' error

--- pet-care/src/components/auth/ProtectedRoute.jsx
+++ pet-care/src/components/auth/ProtectedRoute.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Navigate, useLocation } from "react-router-dom";
+import { Navigate, Outlet, useLocation } from "react-router-dom";
 
 const ProtectedRoute = ({ children, allowedRoles = [], useOutlet = false }) => {
   const loggedIn = localStorage.getItem("authToken");
