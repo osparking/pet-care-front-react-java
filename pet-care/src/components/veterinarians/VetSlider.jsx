feafc1075b4c40d6e37b92d5ce63ef3129fc838b
osparking <jbpark03@gmail.com>
2025-02-18 09:16:44 +0900

    in main, carousel skeleton created

--- pet-care/src/components/veterinarians/VetSlider.jsx
+++ pet-care/src/components/veterinarians/VetSlider.jsx
@@ -1,11 +1,18 @@
-import React from 'react'
+import React from "react";
+import { Carousel } from "react-bootstrap";
 
-const VetSlider = ({vets}) => {
+const VetSlider = ({ vets }) => {
   return (
     <main>
-      
+      <Carousel interval={5000} indicators={true} controls={true}>
+        {vets &&
+          vets.map((vet, index) => 
+            <Carousel.Item key={index}>
+                
+            </Carousel.Item>)}
+      </Carousel>
     </main>
-  )
-}
+  );
+};
 
-export default VetSlider
+export default VetSlider;
