09344f9cffbb5da0ae5404bb655c2eec2a795b03
osparking <jbpark03@gmail.com>
2025-02-18 09:39:07 +0900

    carousel item - photo column

--- pet-care/src/components/veterinarians/VetSlider.jsx
+++ pet-care/src/components/veterinarians/VetSlider.jsx
@@ -6,10 +6,27 @@ const VetSlider = ({ vets }) => {
     <main>
       <Carousel interval={5000} indicators={true} controls={true}>
         {vets &&
-          vets.map((vet, index) => 
+          vets.map((vet, index) => (
             <Carousel.Item key={index}>
-                
-            </Carousel.Item>)}
+              <Row className="align-items-center">
+                <Col xs={12} md={4} className="text-center">
+                  <Card.Img
+                    src={
+                      vet.photo
+                        ? `data:image/png;base64,${vet.photo}`
+                        : placeholderImage
+                    }
+                    alt={"photo"}
+                    style={{
+                      maxWidth: "400px",
+                      maxHeight: "400px",
+                      objectFit: "contain",
+                    }}
+                  />
+                </Col>
+              </Row>
+            </Carousel.Item>
+          ))}
       </Carousel>
     </main>
   );
