ef8bcdeff14f4695d344af8d25c8a0f940aea0d5
osparking <jbpark03@gmail.com>
2025-02-20 08:40:33 +0900

    complete div by inserting Spinner inside

--- pet-care/src/components/common/LoadingSpinner.jsx
+++ pet-care/src/components/common/LoadingSpinner.jsx
@@ -1,11 +1,15 @@
-import React from 'react'
+import React from "react";
+import { Spinner } from "react-bootstrap";
 
-const LoadingSpinner = ({variant = "success"}) => {
+const LoadingSpinner = ({ variant = "success" }) => {
   return (
-    <div>
-      
+    <div
+      className="d-flex justify-content-center align-items-center mt-5"
+      style={{ height: "1005" }}
+    >
+      <Spinner animation="border" variant={variant} />
     </div>
-  )
-}
+  );
+};
 
-export default LoadingSpinner
+export default LoadingSpinner;
