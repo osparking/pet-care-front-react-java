7a40615b2079e02bcfa3277f17bed549dee8b9f4
osparking <jbpark03@gmail.com>
2025-02-21 15:33:55 +0900

    defines password reset request compo'

---
+++ pet-care/src/components/auth/PwdResetRequest.jsx
@@ -0,0 +1,12 @@
+import React, { useState } from "react";
+
+const PwdResetRequest = () => {
+  const [email, setEmail] = useState("");
+  const [isProcessing, setIsProcessing] = useState(false);
+  return (
+    <div>
+    </div>
+  );
+};
+
+export default PwdResetRequest;
