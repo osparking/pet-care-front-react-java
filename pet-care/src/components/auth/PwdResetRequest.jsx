43eeb33e51eb38dd8f0541041dcafa6be4d64a53
osparking <jbpark03@gmail.com>
2025-02-21 15:38:32 +0900

    use message hook and import some names

--- pet-care/src/components/auth/PwdResetRequest.jsx
+++ pet-care/src/components/auth/PwdResetRequest.jsx
@@ -1,8 +1,19 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+import UseMsgAlerts from "../hooks/UseMsgAlerts";
 
 const PwdResetRequest = () => {
   const [email, setEmail] = useState("");
   const [isProcessing, setIsProcessing] = useState(false);
+  const {
+    successMsg,
+    setSuccessMsg,
+    errorMsg,
+    setErrorMsg,
+    showSuccessAlert,
+    setShowSuccessAlert,
+    showErrorAlert,
+    setShowErrorAlert,
+  } = UseMsgAlerts();
   return (
     <div>
     </div>
