cab3eac53743e4677de7a6891a5b5443d778c1f0
osparking <jbpark03@gmail.com>
2025-02-22 09:47:04 +0900

    compo accepts new password after token validated

---
+++ pet-care/src/components/auth/ResetPassword.jsx
@@ -0,0 +1,13 @@
+import React, { useState } from "react";
+
+const ResetPassword = () => {
+  const [newPwd, setNewPwd] = useState("");
+  const [isProcessing, setIsProcessing] = useState(false);
+  const [tokenStatus, setTokenStatus] = useState("보류");
+  return (
+    <div>        
+    </div>
+  )
+};
+
+export default ResetPassword;
