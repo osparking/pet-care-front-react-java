fb0cd9d996e0ee7e408d02ba03ab9761c157b742
osparking <jbpark03@gmail.com>
2025-02-22 09:57:28 +0900

    bring in message hook

--- pet-care/src/components/auth/ResetPassword.jsx
+++ pet-care/src/components/auth/ResetPassword.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import UseMsgAlerts from "../hooks/UseMsgAlerts";
 
 const ResetPassword = () => {
   const [newPwd, setNewPwd] = useState("");
@@ -6,7 +7,17 @@ const ResetPassword = () => {
   const [tokenStatus, setTokenStatus] = useState("보류");
   const queryParams = new URLSearchParams(window.location.search);
   const token = queryParams.get("token");
-  
+  const {
+    successMsg,
+    setSuccessMsg,
+    errorMsg,
+    setErrorMsg,
+    showSuccessAlert,
+    setShowSuccessAlert,
+    showErrorAlert,
+    setShowErrorAlert,
+  } = UseMsgAlerts();
+
   return (
     <div>        
     </div>
