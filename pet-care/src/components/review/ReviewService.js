bd861ad8ccfd7220d505b0eecb997f2df9d6d26f
osparking <jbpark03@gmail.com>
2025-02-24 17:53:37 +0900

    send token with review save request

--- pet-care/src/components/review/ReviewService.js
+++ pet-care/src/components/review/ReviewService.js
@@ -2,9 +2,11 @@ import { api } from "../utils/api";
 export async function saveReview(patId, vetId, review) {
   try {
     console.log("ID, ID, Review", patId, vetId, review);
+    const token = localStorage.getItem("authToken");
     const response = await api.post(
       `reviews/create?patientId=${patId}&vetId=${vetId}`,
-      review
+      review,
+      { headers: { Authorization: `Bearer ${token}` } }
     );
     return response.data;
   } catch (error) {
